910d7169bf0ec1d11c075a7a791dbf9032d7baea
dstavrov <danila.stavrov@intel.com>
2020-02-13 19:49:13 +0300

    Fix permission denied

--- dynamic/build_rpms.sh
+++ dynamic/build_rpms.sh
@@ -10,15 +10,15 @@ names=($1 $2 $2)
 TMP="test"
 
 for rpmname in "${names[@]}"; do
-  echo "   - label: \":Build RPM: ${rpmname}\"
-    key: \"build_rpm_${rpmname}\"
-    plugins:
-      - ./plugins/rpmbuild:
-          spec: \"./hello_world.spec\"
-          share: \"${TMP}\"
-    agents:
-      - \"rpmbuild=true\"
-    
+  echo "  - label: \":Build RPM: ${rpmname}\"
+  key: \"build_rpm_${rpmname}\"
+  plugins:
+    - ./plugins/rpmbuild:
+        spec: \"./hello_world.spec\"
+        share: \"${TMP}\"
+  agents:
+    - \"rpmbuild=true\"
+
     "
 done
 
