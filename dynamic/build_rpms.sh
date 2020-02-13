80d6ed567da23c48686617c3072c03eda1510c17
dstavrov <danila.stavrov@intel.com>
2020-02-13 16:33:01 +0300

    Add Build RPM Plugin

---
+++ dynamic/build_rpms.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# exit immediately on failure, or if an undefined variable is used
+set -eu
+
+# begin the pipeline.yml file
+echo "steps:"
+
+names=($1 $2 $2)
+TMP="test"
+
+for rpmname in "${names[@]}"; do
+  echo "   - label: \":Build RPM: ${rpmname}\"
+    key: \"build_rpm_${rpmname}\"
+    plugins:
+      - ./plugins/rpmbuild:
+          spec: \"./hello_world.spec\"
+          share: \"${TMP}\"
+    agents:
+      - \"rpmbuild=true\"
+    
+    "
+done
+
