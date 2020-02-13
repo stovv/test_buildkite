77658800a56096dafdb9576ac8d3fccb3858b3d5
dstavrov <danila.stavrov@intel.com>
2020-02-13 21:18:06 +0300

    TEST

--- dynamic/build_rpms.sh
+++ dynamic/build_rpms.sh
@@ -10,14 +10,15 @@ names=($1 $2 $2)
 TMP="test"
 
 for rpmname in "${names[@]}"; do
-  echo "  - label: \":Build RPM: ${rpmname}\"
-  key: \"build_rpm_${rpmname}\"
-  plugins:
+  echo "
+  - label: \":build: Build RPM ${rpmname}\"
+    key: \"build_rpm_${rpmname}\"
+    plugins:
     - ./plugins/rpmbuild:
         spec: \"./hello_world.spec\"
-        share: \"${TMP}\"
-  agents:
-    - \"rpmbuild=true\"
+        share: \"test\"
+    agents:
+      - \"rpmbuild=true\"
 
     "
 done
